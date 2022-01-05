cd20f37745b098513972523cbabc46b097446a90
mjBerhe <mjberhe123@hotmail.com>
2022-01-05 16:12:56 -0500

    meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Skills from "../components/Skills";
 
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import About from "../components/About";
 import Works from "../components/Works";
 import Contact from "../components/Contact";
@@ -55,28 +56,34 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="page-container">
-      <div className="stars"></div>
-      <div className="stars2"></div>
-      <div className="stars3"></div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/logos/corners.png" />
+        <title>Matthew Berhe</title>
+        <meta name="description" content="Web Portfolio of Matthew Berhe" />
+      </Head>
+      <div className="page-container">
+        <div className="stars"></div>
+        <div className="stars2"></div>
+        <div className="stars3"></div>
 
-      <div className="max-w-7xl mx-auto min-h-screen flex flex-col">
-        <div className="flex flex-col h-screen">
-          <nav className="hidden sm:flex justify-around mx-6 sm:mx-12 md:mx-24 lg:mx-48 mt-4 text-3xl font-bold tracking-tight slide-down-delay">
-            {navLinks.map((navItem) => (
-              <button
-                key={navItem.name}
-                className="nav-link text-3xl font-bold tracking-tight"
-                onClick={() => scrollToRef(navItem.ref)}
-              >
-                {navItem.name}
-              </button>
-            ))}
-          </nav>
+        <div className="max-w-7xl mx-auto min-h-screen flex flex-col">
+          <div className="flex flex-col h-screen">
+            <nav className="hidden sm:flex justify-around mx-6 sm:mx-12 md:mx-24 lg:mx-48 mt-4 text-3xl font-bold tracking-tight slide-down-delay">
+              {navLinks.map((navItem) => (
+                <button
+                  key={navItem.name}
+                  className="nav-link text-3xl font-bold tracking-tight"
+                  onClick={() => scrollToRef(navItem.ref)}
+                >
+                  {navItem.name}
+                </button>
+              ))}
+            </nav>
 
-          <div className="flex flex-1 flex-col items-start justify-center">
-            <div className="flex">
-              {/* <div className="flex flex-col space-y-3">
+            <div className="flex flex-1 flex-col items-start justify-center">
+              <div className="flex">
+                {/* <div className="flex flex-col space-y-3">
                 {profileLinks.map((link) => (
                   <img
                     key={link.name}
@@ -86,35 +93,38 @@ const Home: NextPage = () => {
                   />
                 ))}
               </div> */}
-              <div className="flex flex-col px-4">
-                <h1 className="main-title font-bold slide-in">Matthew Berhe</h1>
-                <h2 className="text-2xl font-medium text-gray-400 slide-down">
-                  Full Stack Developer
-                </h2>
+                <div className="flex flex-col px-4">
+                  <h1 className="main-title font-bold slide-in">
+                    Matthew Berhe
+                  </h1>
+                  <h2 className="text-2xl font-medium text-gray-400 slide-down">
+                    Full Stack Developer
+                  </h2>
+                </div>
               </div>
             </div>
           </div>
+
+          <div ref={aboutRef}>
+            <About />
+          </div>
+
+          <div ref={skillsRef}>
+            <Skills />
+          </div>
+
+          <div ref={worksRef}>
+            <Works />
+          </div>
+
+          <div ref={contactRef}>
+            <Contact />
+          </div>
         </div>
 
-        <div ref={aboutRef}>
-          <About />
-        </div>
-
-        <div ref={skillsRef}>
-          <Skills />
-        </div>
-
-        <div ref={worksRef}>
-          <Works />
-        </div>
-
-        <div ref={contactRef}>
-          <Contact />
-        </div>
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
