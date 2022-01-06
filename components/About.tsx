5507e7dc855b754ee0a1564857c724d13ab4278a
mjBerhe <mjberhe123@hotmail.com>
2022-01-05 21:42:49 -0500

    font size test

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
       <div className="flex flex-col flex-1 space-y-6">
         <div className="self-start relative mt-24 mx-12">
           <div className="text-overline"></div>
-          <h1 className="text-3xl sm:text-4xl font-bold text-left mt-4">
+          <h1 className="text-3xl sm:text-2xl font-bold text-left mt-4">
             ABOUT ME
           </h1>
         </div>
@@ -36,12 +36,12 @@ const About = () => {
             <div className="border-br-up"></div>
             <div className="border-br-left"></div>
             <div className="flex flex-col space-y-8 fade-in-delay">
-              <p className="text-xl sm:text-3xl font-medium">
+              <p className="text-xl sm:text-2xl">
                 Hey! I&apos;m Matthew, a self-taught developer based in Toronto.
                 I love building websites that provide utility for the user,
                 while leaving them with an enjoyable experience.
               </p>
-              <p className="text-xl sm:text-3xl font-medium">
+              <p className="text-xl sm:text-2xl">
                 I&apos;m obsessed with both video games and efficiency, which is
                 why I was inspired to learn programming. Building tools to make
                 my life easier feels natural, and extending that to the world is
